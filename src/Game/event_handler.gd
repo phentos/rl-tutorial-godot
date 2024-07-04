cff053be00d0415827428c9736e35f3e6fb158e8
Korey Ray <phentos@gmail.com>
2024-07-03 21:27:07 -0700

    sync

---
+++ src/Game/event_handler.gd
@@ -0,0 +1,17 @@
+class_name EventHandler
+extends Node
+
+var events = {
+	"ui_up":	MovementAction.new( 0,-1),
+	"ui_down":	MovementAction.new( 0, 1),
+	"ui_left":	MovementAction.new(-1, 0),
+	"ui_right":	MovementAction.new( 1, 0),
+	"ui_cancel":EscapeAction.new()
+}
+
+func get_action() -> Action:
+	for e in events.keys():
+		if Input.is_action_just_pressed(e):
+			return events[e]
+	
+	return null
