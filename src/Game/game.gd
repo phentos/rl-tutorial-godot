6ee89de0053e6a7021c979915ba5b54d8455def6
Korey Ray <phentos@gmail.com>
2024-07-04 20:48:03 -0700

    eod up to

--- src/Game/game.gd
+++ src/Game/game.gd
@@ -1,20 +1,31 @@
 extends Node2D
 
-var player_grid_pos := Vector2i.ZERO
+const player_definition: EntityDefinition = preload("res://assets/definitions/entities/actors/entity_definition_player.tres")
 
-@onready var player: Sprite2D = $Player
+@onready var player: Entity
 @onready var event_handler: EventHandler = $EventHandler
+@onready var entities: Node2D = $Entities
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var player_start_pos: Vector2i = Grid.world_to_grid(get_viewport_rect().size.floor() / 2)
+	player = Entity.new(player_start_pos, player_definition)
+	
+	var npc := Entity.new(player_start_pos + Vector2i.RIGHT, player_definition)
+	npc.modulate = Color.ORANGE_RED
+	
+	for e in [player, npc]:
+		entities.add_child(e)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	var action: Action = event_handler.get_action()
-	
-	if action is MovementAction:
-		player_grid_pos += action.offset
-		player.position = Grid.grid_to_world(player_grid_pos)
-	elif action is EscapeAction:
-			get_tree().quit()
+	# TODO refactor for entdef
+	#var action: Action = event_handler.get_action()
+	#
+	#if action is MovementAction:
+		#player_grid_pos += action.offset
+		#player.position = Grid.grid_to_world(player_grid_pos)
+	#elif action is EscapeAction:
+			#get_tree().quit()
+	pass
